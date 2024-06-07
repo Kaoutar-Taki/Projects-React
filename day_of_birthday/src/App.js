1cce2bbf6461d2c26750f4352e6aed6b97c0041e
Kaoutar-Taki <takikaoutar01@gmail.com>
2024-06-07 22:32:13 +0100

    install tailwind css

--- day_of_birthday/src/App.js
+++ day_of_birthday/src/App.js
@@ -1,7 +1,19 @@
+import React from 'react';
+
 function App() {
   return (
-    <>
-    </>
+    <div className="App">
+      <header className="bg-blue-500 text-white p-4">
+        <h1 className="text-3xl font-bold">
+          Welcome to React with Tailwind CSS
+        </h1>
+      </header>
+      <main className="p-4">
+        <p className="text-lg">
+          This is a simple example of using Tailwind CSS in a React project.
+        </p>
+      </main>
+    </div>
   );
 }
 

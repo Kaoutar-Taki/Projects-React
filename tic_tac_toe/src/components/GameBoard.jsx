9778f27beedd5c838e3b6e17a759ded6acf703e6
Kaoutar-Taki <takikaoutar01@gmail.com>
2024-06-26 14:47:59 +0100

    GameBoard v1

---
+++ tic_tac_toe/src/components/GameBoard.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+const GameBoard = ({ onSelectSquare, activePlayerSymbol }) => {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+    onSelectSquare();
+  };
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <ol key={rowIndex}>
+          {row.map((playerSymbol, colIndex) => (
+            <li key={colIndex}>
+              <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                {playerSymbol}
+              </button>
+            </li>
+          ))}
+        </ol>
+      ))}
+    </ol>
+  );
+};
+
+GameBoard.propTypes = {
+  onSelectSquare: PropTypes.func.isRequired,
+  activePlayerSymbol: PropTypes.string.isRequired,
+};
+export default GameBoard;
+

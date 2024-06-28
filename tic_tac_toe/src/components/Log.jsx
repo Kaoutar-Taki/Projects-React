32d38976d7fccb90f080d1ae4bd584919c619f70
Kaoutar-Taki <takikaoutar01@gmail.com>
2024-06-28 13:12:23 +0100

    COMPLETTING Log

--- tic_tac_toe/src/components/Log.jsx
+++ tic_tac_toe/src/components/Log.jsx
@@ -1,6 +1,19 @@
-const Log = () => {
-  return <div></div>;
+import PropTypes from "prop-types";
+
+const Log = ({ turns }) => {
+  return (
+    <ol id="log">
+      {turns.map((turn) => (
+        <li key={`${turn.square.row}${turn.square.col}`}>
+          {turn.player} selected {turn.square.row},{turn.square.col}
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+Log.propTypes = {
+  turns: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default Log;
-

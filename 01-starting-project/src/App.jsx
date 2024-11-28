4f1f21ffec0a4f37d74b6fbe15a0d067297e6f91
Kaoutar-Taki <takikaoutar01@gmail.com>
2024-11-28 16:42:32 +0100

    Add TimerChallenge component with varying difficulty levels to App

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,12 +1,20 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
+import TimerChallenge from "./components/TimerChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={5} />
+        <TimerChallenge title="Not easy" targetTime={10} />
+        <TimerChallenge title="Getting tough" targetTime={15} />
+        <TimerChallenge title="Pros only" targetTime={20} />
+        <TimerChallenge title="Extremely difficult" targetTime={1} />
+      </div>
     </>
   );
 }
 
 export default App;
+

08bccc877fd33ea30fde5016c547b003d4a2b327
Kaoutar-Taki <takikaoutar01@gmail.com>
2024-11-30 11:08:38 +0100

    Implement project addition functionality and refactor Input component to use refs

--- project_management/src/components/NewProject.jsx
+++ project_management/src/components/NewProject.jsx
@@ -1,6 +1,24 @@
+import { useRef } from "react";
 import Input from "./Input.jsx";
 
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
+  const titleInputRef = useRef();
+  const descriptionInputRef = useRef();
+  const dueDateInputRef = useRef();
+
+  function handleSave() {
+    const enteredTitle = titleInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+    const enteredDueDate = dueDateInputRef.current.value;
+
+    // validation...
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -8,15 +26,18 @@ export default function NewProject() {
           <button className="text-stone-800 hover:text-stone-950">
             Cancel
           </button>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}
+          >
             Save
           </button>
         </li>
       </menu>
       <div>
-        <Input label="Title" />
-        <Input label="Description" textarea />
-        <Input label="Due Date" type="date" />
+        <Input ref={titleInputRef} label="Title" />
+        <Input ref={descriptionInputRef} label="Description" textarea />
+        <Input ref={dueDateInputRef} label="Due Date" type="date" />
       </div>
     </div>
   );
